6504eb054cedfee8b1b0404384b1173f453d982c
Ismael Cabañas García <ismael.cabanas@adevinta.com>
2020-12-26 09:38:41 +0100

    Some improves

--- storeroom-web/src/acceptance-test/kotlin/cabanas/garcia/ismael/storeroom/AcceptanceTest.kt
+++ storeroom-web/src/acceptance-test/kotlin/cabanas/garcia/ismael/storeroom/AcceptanceTest.kt
@@ -4,16 +4,13 @@ import cabanas.garcia.ismael.storeroom.infrastructure.framework.Application
 import cabanas.garcia.ismael.storeroom.testcontainers.TestcontainersInitializer
 import io.restassured.module.mockmvc.RestAssuredMockMvc
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 
-@ExtendWith(SpringExtension::class)
 @SpringBootTest(
         classes = [Application::class]
 )
