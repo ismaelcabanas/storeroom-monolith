d9c31a1ef4545e1e50b49f5a275ff385ff66376c
Ismael Cabañas García <ismael.cabanas@adevinta.com>
2021-01-08 17:33:18 +0100

    Improve testing code

--- storeroom-web/src/test/kotlin/cabanas/garcia/ismael/storeroom/domain/shoppinglist/ShoppingListShould.kt
+++ storeroom-web/src/test/kotlin/cabanas/garcia/ismael/storeroom/domain/shoppinglist/ShoppingListShould.kt
@@ -7,29 +7,29 @@ import org.junit.jupiter.api.Test
 class ShoppingListShould {
     @Test
     fun `create a shopping list for a storeroom`() {
-        val sut = ShoppingList.create(SOME_SHOPPING_LIST_ID, SOME_STOREROOM_ID, SOME_OWNER_ID)
+        val actualShoppingList = ShoppingList.create(SOME_SHOPPING_LIST_ID, SOME_STOREROOM_ID, SOME_OWNER_ID)
 
-        val expected = ShoppingList(SOME_SHOPPING_LIST_ID, SOME_STOREROOM_ID, SOME_OWNER_ID)
-        sut shouldBe expected
+        val expectedShoppingList = ShoppingList(SOME_SHOPPING_LIST_ID, SOME_STOREROOM_ID, SOME_OWNER_ID)
+        actualShoppingList shouldBe expectedShoppingList
     }
 
     @Test
     fun `add new product to shopping list`() {
-        val sut = ShoppingList.create(SOME_SHOPPING_LIST_ID, SOME_STOREROOM_ID, SOME_OWNER_ID)
+        val shoppingList = ShoppingList.create(SOME_SHOPPING_LIST_ID, SOME_STOREROOM_ID, SOME_OWNER_ID)
 
-        val actual = sut.addProduct(Product(ProductId(SOME_PRODUCT_ID), SOME_PRODUCT_NAME))
+        val actualShoppingList = shoppingList.addProduct(Product(ProductId(SOME_PRODUCT_ID), SOME_PRODUCT_NAME))
 
-        val expected = ShoppingList(SOME_SHOPPING_LIST_ID, SOME_STOREROOM_ID, SOME_OWNER_ID, listOf(Product(ProductId(SOME_PRODUCT_ID), SOME_PRODUCT_NAME)))
-        actual shouldBe expected
+        val expectedShoppingList = ShoppingList(SOME_SHOPPING_LIST_ID, SOME_STOREROOM_ID, SOME_OWNER_ID, listOf(Product(ProductId(SOME_PRODUCT_ID), SOME_PRODUCT_NAME)))
+        actualShoppingList shouldBe expectedShoppingList
     }
 
     @Test
     fun `throws product does not exists error when adds a existent product in shopping list`() {
         val someProduct = Product(ProductId(SOME_PRODUCT_ID), SOME_PRODUCT_NAME)
-        val sut = ShoppingListMother.createShoppingListWithProducts(listOf(someProduct))
+        val shoppingList = ShoppingListMother.createShoppingListWithProducts(listOf(someProduct))
 
         val exception = shouldThrow<ProductAlreadyExitsException> {
-            sut.addProduct(Product(ProductId(SOME_PRODUCT_ID), SOME_PRODUCT_NAME))
+            shoppingList.addProduct(Product(ProductId(SOME_PRODUCT_ID), SOME_PRODUCT_NAME))
         }
         exception.message shouldBe "Product '$SOME_PRODUCT_ID' already exist in the shopping list"
     }
