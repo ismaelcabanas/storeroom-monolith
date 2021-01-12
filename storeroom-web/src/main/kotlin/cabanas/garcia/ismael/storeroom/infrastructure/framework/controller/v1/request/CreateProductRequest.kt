80e25135e0b6850190e8dd2f1fa8f6dfd6fa7795
Ismael Cabañas García <ismael.cabanas@adevinta.com>
2021-01-12 09:03:18 +0100

    Move product catalog controller and delete unit test temporally

--- storeroom-web/src/main/kotlin/cabanas/garcia/ismael/storeroom/infrastructure/framework/controller/v1/request/CreateProductRequest.kt
+++
@@ -1,9 +0,0 @@
-package cabanas.garcia.ismael.storeroom.infrastructure.framework.controller.v1.request
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class CreateProductRequest (
-        @JsonProperty("productId") val productId: String,
-        @JsonProperty("productName") val productName: String
-)
-
